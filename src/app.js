111fc83503850e34c192a97f3c58f50012088df1
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-06-06 01:20:39 +0000

    done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const at = [".com", ".eu", ".us"];
+
+  function domainNameGenerator() {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < at.length; l++) {
+            console.log(concatString(pronoun[i], adj[j], noun[k], at[l]));
+          }
+        }
+      }
+    }
+  }
+
+  function concatString(str1, str2, str3, str4) {
+    return `${str1}${str2}${str3}${str4}`;
+  }
+
+  domainNameGenerator();
 };
